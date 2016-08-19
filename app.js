7e2cd4605d9cfce1cf1ecf45c47371bec7d143e1
jonathan.brooks <jonathan.brooks@readingroom.com>
2016-08-19 17:33:13 +0100

    ~ Added partioning for sprite creation

--- app.js
+++ app.js
@@ -17,14 +17,12 @@ function getSequences(sequences) {
 }
 
 function generateFlick (seq) {
-	let srcSprite = `${seq}_sprite.jpg`;
 	return new Promise((resolve, reject) => {
 		getFilenames(seq) // get all the files in the target directory
 		.then(fileNames => fileNames.filter((f, i) => /.jpg/.test(f))) // get the images of the sequence
 		.then(imageNames => imageNames.sort()) // sort them alpabetically
 		.then(sortedImageNames => sortedImageNames.map(s => imglib(`sequences/${seq}/` + s))) // get an array of image objects from the filenames
-		.then(images => createSprite(images, srcSprite)) // create the sprite		
-		.then(sprite => sprite.save(`flicker/${srcSprite}`)) // save the sprite
+		.then(images => createSprite(images, seq)) // create the sprite		
 		.then(resolve) // then resolve the promise
 		.catch(e => reject(e)) // otherwise reject with caugh error
 	})	
@@ -39,31 +37,46 @@ function getFilenames(seq) {
 	})
 }
 
-function createSprite(images, srcSprite) {
-	let w = images[0].width(), 
-		h =	images[0].height(),
-		columns = Math.ceil(10000 / w), // get number of columns
-		rows = Math.ceil(images.length / columns); // get number of rows
+function createSprite(images, seq) {
+	let w = images[0].width(), // get the unit width of each image
+		h =	images[0].height(), // get the unit height of each iamge
+		maxCols = Math.ceil(4096 / w), // get max columns per sprite
+		maxRows = Math.ceil(4096 / h), // get max rows per sprite
+		cols = maxCols, // as well as attributes for columns
+		rows = maxRows, // and rows
+		iteration = 0, // and the sequence iteration
+		sprite; // declare the empty sprite
 
-	let sprite = imglib(columns * w, rows * h); // generate the empty sprite
-	let pos = { col: 0, row: 0 }; // remember position in the grid	
+	let pos = { col: 0, row: 0 }; // remember position in the grid
+	let spritesNeeded = Math.ceil(images.length / (maxCols * maxRows));
+	let finalSprite = Math.ceil(images.length % (maxCols * maxRows));
 
-	images.forEach(img => {		
-		map.frames.push({src: srcSprite, x: w * pos.col, y: h * pos.row }); // add this frame to the animation frames object
-		sprite.draw(img, w * pos.col, h * pos.row); // draw this image to the sprite		
-
-		if(pos.col + 1 > columns - 1 && pos.row + 1 > rows) { // if the position exceeds the grid
-			throw new Error('Sprite has exceeded maximum dimensions - oops!'); // throw an error
+	images.forEach((img, i) => {
+		if(i % (maxCols * maxRows) === 0) { // if this is a new sprite
+			if(--spritesNeeded === 0) { // and this is the last sprite
+				cols = finalSprite < maxCols ? Math.ceil(finalSprite % maxCols) : maxCols; // calc cols in the sprite
+				rows = Math.ceil(finalSprite / cols); // and then the rows
+			}
+			pos = { col: 0, row: 0 }; // reset position in the grid
+			sprite = imglib(cols * w, rows * h); // create the sprite
 		}
-		else if (pos.col + 1 > columns - 1) { // if we're at the end of the row
+		
+		map.frames.push({src: `${seq}_${iteration}.sprite.jpg`, x: w * pos.col, y: h * pos.row }); // add this frame to the animation frames object
+		sprite.draw(img, w * pos.col, h * pos.row); // draw this image to the sprite
+
+		if (pos.col + 1 > cols - 1) { // if we're at the end of the row
 			pos.row++; // move to the next row
 			pos.col = 0; // reset to the first column
 		}
 		else {
 			pos.col++; // otherwise just move to the next column
 		}
+		if(i !== 0 && (i % ((maxCols * maxRows) - 1) === 0 || i === images.length - 1)) {
+			console.log(`saving sprite: ${seq}_${iteration}.sprite.jpg`);
+			sprite.save(`flicker/${seq}_${iteration}.sprite.jpg`);
+			iteration++;
+		}
 	})
-	return sprite; // return the completed sprite
 }
 
 function saveMap() {
